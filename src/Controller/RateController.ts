45ab4ffee375d5b5762aeddc5a7c7726071e963e
Ahmed-Atef-Adel <Ahmed-Atef-Adel.ahmedatef8885@gmail.com>
2025-02-14 20:47:52 +0200

    Data

--- src/Controller/RateController.ts
+++ src/Controller/RateController.ts
@@ -4,8 +4,8 @@ import { AppDataSource } from "../dbConfig/data-source";
 class RateController {
   static getRate = async (request: any, response: Response): Promise<void> => {
     try {
-      const rateReposiory = AppDataSource.getRepository("rate");
-      const rates = await rateReposiory.find();
+      const rateRepository = AppDataSource.getRepository("rate");
+      const rates = await rateRepository.find();
       response.json(rates);
     } catch (error) {
       response.status(500).json({ message: error });
@@ -14,8 +14,20 @@ class RateController {
 
   static addRate = async (request: any, response: Response): Promise<void> => {
     try {
-      const rateReposiory = AppDataSource.getRepository("rate");
-      const rates = await rateReposiory.save(request.body);
+      const rateRepository = AppDataSource.getRepository("rate");
+      const { userId, productId, rate, comment } = request.body;
+      const existingRate = await rateRepository.findOne({
+        where: {
+          userId: request.body.userId,
+          productId: request.body.productId,
+        },
+      });
+      if (existingRate) {
+        response.status(400).json({
+          message: "You have already rated this product before.",
+        });
+      }
+      const rates = await rateRepository.save(request.body);
       response.json(rates);
     } catch (error) {
       response.status(500).json({ message: error });
@@ -27,11 +39,11 @@ class RateController {
     response: Response
   ): Promise<void> => {
     try {
-      const rateReposiory = AppDataSource.getRepository("rate");
-      const rates = await rateReposiory.findOne({
+      const rateRepository = AppDataSource.getRepository("rate");
+      const rates = await rateRepository.findOne({
         where: { id: request.params.id },
       });
-      const updatedRate = await rateReposiory.save({
+      const updatedRate = await rateRepository.save({
         ...rates,
         ...request.body,
       });
@@ -46,8 +58,8 @@ class RateController {
     response: Response
   ): Promise<void> => {
     try {
-      const rateReposiory = AppDataSource.getRepository("rate");
-      const rates = await rateReposiory.delete({ id: request.params.id });
+      const rateRepository = AppDataSource.getRepository("rate");
+      const rates = await rateRepository.delete({ id: request.params.id });
       response.json({ message: "Rate deleted successfully." });
     } catch (error) {
       response.status(500).json({ message: error });
